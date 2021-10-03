7a71cacb7d7c4a6f21331045c2814b530c65e648
omegafrog <jiwoo22110103@gmail.com>
2021-10-03 14:43:58 +0900

    배경화면 이미지가 변경되는게 아니라 새로 추가되는 오류 고침 html에 빈 이미지 태그를 만들고 소스를 변경하도록

--- src/background.js
+++ src/background.js
@@ -1,8 +1,9 @@
-const bgs = ["./img/0.jpg","./img/1.jpg","./img/2.jpg","./img/3.jpg","./img/4.jpg","./img/5.jpg","./img/6.jpg","./img/7.jpg","./img/8.jpg","./img/9.jpg"]
+const bgs = ["./img/0.jpg","./img/1.jpg","./img/2.jpg","./img/3.jpg","./img/4.jpg","./img/5.jpg","./img/6.jpg","./img/7.jpg","./img/8.jpg","./img/9.jpg"];
 function changeBg(){
     const bgIndex = Math.ceil(Math.random()*(bgs.length-1));
-    const bg = document.createElement('img');
+    const bg = document.querySelector(".background");
     bg.src=bgs[bgIndex];
+    bg.id = bgIndex;
     document.body.appendChild(bg);
 }
 changeBg();
