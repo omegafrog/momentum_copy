fc42af8b30fd8941acb66264341388d1f0f73ccc
omegafrog <jiwoo22110103@gmail.com>
2021-10-03 15:20:59 +0900

    if user in localstorage, after login div not hidden after login, afterLogin div is not hidden

--- src/greetings.js
+++ src/greetings.js
@@ -1,29 +1,33 @@
 const loginForm = document.querySelector(".js-loginForm");
 const greeting = document.querySelector(".js-greeting");
 const userNameInput = loginForm.querySelector("input");
+const afterLoginDiv = document.querySelector("#after-login");
 
 const STORAGE_USERNAME_KEY = "name";
 
-function showGreeting(userName){
+function showGreeting(userName) {
     greeting.innerHTML = `Hello, ${userName}!`;
     greeting.hidden = false;
     loginForm.hidden = true;
 }
-function newUser(){
+function newUser() {
     const userName = userNameInput.value;
     localStorage.setItem(STORAGE_USERNAME_KEY, userName);
     return userName;
 }
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
     showGreeting(newUser());
+    afterLoginDiv.hidden = false;
 }
 loginForm.addEventListener("submit", handleSubmit);
 const currentUser = localStorage.getItem(STORAGE_USERNAME_KEY);
-    if(!currentUser){
-        greeting.hidden = true;
-        loginForm.hidden = false;
-    }
-    else{
-        showGreeting(currentUser);
-    }
+if (!currentUser) {
+    greeting.hidden = true;
+    loginForm.hidden = false;
+    afterLoginDiv.hidden = true;
+}
+else {
+    afterLoginDiv.hidden = false;
+    showGreeting(currentUser);
+}
